665ac814613aca0b70b353e32b85ae4d4b7494f9
Elena Stupina <Elena_Stupina@epam.com>
2023-07-06 20:22:29 +0300

    Issue-79: Update MSS Content Schema
    Update MSS Content Schema

--- tests/test_api/test_routes/multistageseparator/mss_test_mock_objects.py
+++ tests/test_api/test_routes/multistageseparator/mss_test_mock_objects.py
@@ -110,7 +110,7 @@ INCORRECT_SCHEMA_TEST_DATA = {
 
 INCORRECT_DATAFRAME_TEST_DATA = copy.deepcopy(TEST_DATA)
 INCORRECT_DATAFRAME_TEST_DATA["data"][0].pop()  # deleting MultiStageSeparatorTestSteps
-EXPECTED_ERROR_REASON = "Data error: 12 columns passed, passed data had 11 columns"
+EXPECTED_ERROR_REASON = "Data error: 15 columns passed, passed data had 14 columns"
 
 
 TEST_WRONG_COLUMNS_FILTERS = [
