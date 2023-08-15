2e2a172d5626a0dbac29f3c20526bb68747ddd11
Arturo Hernandez [EPAM] <arturo_hernandez@epam.com>
2023-08-15 12:38:17 +0000

    Introduce rafs app logic to client class

--- client/api/storage.py
+++ client/api/storage.py
@@ -1,14 +1,15 @@
 from loguru import logger
 from starlette import status
 
+from client.api.settings import ApiClientSettings
 from client.api_client import APIClient
 
 
 class APIStorage(APIClient):
     """API storage service methods."""
 
-    def __init__(self, host: str, data_partition: str, token: str, url_prefix: str = "api/storage/v2") -> None:
-        super().__init__(host, url_prefix, data_partition, token)
+    def __init__(self, data_partition: str, token: str) -> None:
+        super().__init__(ApiClientSettings().service_host_storage, "", data_partition, token)
 
     def purge_record(self, record_id: str, **kwargs) -> None:
         """Method to delete record completely from storage service."""
