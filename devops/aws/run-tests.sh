eefc781badb0c20e3d1fbaab4694cace922b358c
Madalyn Marabella <mmarabe@amazon.com>
2023-11-03 20:51:39 +0000

    Add rafs DDMS integration tests and build test image in buildspec

---
+++ devops/aws/run-tests.sh
@@ -0,0 +1,56 @@
+wget -O /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x /usr/local/bin/jq
+
+export ACCESS_TOKEN=$(curl --location --request POST "https://cognito-idp.${AWS_REGION}.amazonaws.com/" \
+     --header 'X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth' \
+     --header 'Content-Type: application/x-amz-json-1.1' \
+     --data-raw "{ \
+         \"AuthFlow\": \"${AWS_COGNITO_AUTH_FLOW}\", \
+         \"AuthParameters\": { \
+             \"PASSWORD\": \"${ADMIN_PASSWORD}\", \
+             \"USERNAME\": \"${ADMIN_USER}\" \
+         }, \
+         \"ClientId\": \"${AWS_COGNITO_CLIENT_ID}\" \
+	     }" | jq '.AuthenticationResult.AccessToken')
+
+# Set variables
+BUILD_DIR=artifacts/os-rafs-ddms-services
+TEST_DIR=$BUILD_DIR/testing/integration/build-aws
+PARTITION=opendes
+URL_PREFIX=api/rafs-ddms
+CLOUD_PROVIDER=aws
+DDMS_BASE_URL=$HOST
+
+# Parse container image from build-info.json
+TEST_IMAGE=$(jq -r .artifacts[1] $BUILD_DIR/build-info.json)
+echo Running with image $TEST_IMAGE
+
+# Create env file
+cat > .env <<EOF
+SERVICE_NAME=rafs-ddms
+OPENAPI_PREFIX=/api/rafs-ddms
+CLOUD_PROVIDER=aws
+SERVICE_HOST_STORAGE=$DDMS_BASE_URL/api/storage/v2
+SERVICE_HOST_SEARCH=$DDMS_BASE_URL/api/search/v2
+SERVICE_HOST_PARTITION=$DDMS_BASE_URL/api/partition/v1
+SERVICE_HOST_DATASET=$DDMS_BASE_URL/api/dataset/v1
+SERVICE_HOST_LEGAL=$DDMS_BASE_URL/api/legal/v1
+CACHE_ENABLE=False
+URL_PREFIX=api/rafs-ddms
+PARTITION=opendes
+EOF
+
+# Log in to ECR
+ECR_REPO=$(cut -d '/' -f 1 <<< "$TEST_IMAGE")
+ECR_REGION=$(cut -d '.' -f 4 <<< "$ECR_REPO")
+aws ecr get-login-password --region $ECR_REGION | docker login --username AWS --password-stdin $ECR_REPO
+
+# Run integration tests
+echo "ls test dir"
+ls $(pwd)/$TEST_DIR/tests/
+docker run \
+    --env-file .env \
+    -v $(pwd)/$TEST_DIR/tests/:/tests \
+    -v $(pwd)/$TEST_DIR/client/:/client \
+    -w / \
+    $TEST_IMAGE bash -c "pytest -n auto --cov=app --cov-report=term ./tests/integration/tests --ddms-base-url $DDMS_BASE_URL --url-prefix $URL_PREFIX --partition $PARTITION --bearer-token $ACCESS_TOKEN --cloud-provider $CLOUD_PROVIDER"
