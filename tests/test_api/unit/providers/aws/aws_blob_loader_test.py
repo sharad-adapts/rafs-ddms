173f78bcbcd3073577b30320e70340072c1ed30e
Jiman Kim <kimjiman@amazon.com>
2024-02-26 13:00:32 +0000

    Aws Unit Tests

--- tests/test_api/unit/providers/aws/aws_blob_loader_test.py
+++ tests/test_api/unit/providers/aws/aws_blob_loader_test.py
@@ -12,67 +12,88 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from datetime import datetime, timezone, timedelta
 import io
 import json
+from unittest.mock import MagicMock, patch
 
 import boto3
 import pytest
 from botocore.response import StreamingBody
 from botocore.stub import Stubber
 
-from app.providers.dependencies.aws.blob_loader import AWSBlobLoader
+from app.providers.dependencies.aws.blob_loader import CREDENTIALS_CACHE, AWSBlobLoader
 
 TEST_BUCKET = "test-bucket"
 TEST_KEY = "opendes/abc/123"
 TEST_BLOB_URL = f"https://{TEST_BUCKET}.s3.us-east-1.amazonaws.com/{TEST_KEY}?Param=p"
 TEST_BLOB = b"blob"
 
-expected_message = {
-    "message": "blob"
-}
-
-encoded_message = json.dumps(expected_message).encode()
-raw_stream = StreamingBody(
-    io.BytesIO(encoded_message),
-    len(encoded_message)
-)
-
-get_response = {
-    "Body": raw_stream
-}
-
-put_response = {
-    "ETag": "6805f2cfc46c0f04559748bb039d69ae",
-    "VersionId": "Bvq0EDKxOcXLJXNo_Lkz37eM3R4pfzyQ",
-    "ResponseMetadata": {
-        "RequestId": "abc123",
-        "HTTPStatusCode": 200,
-        "HostId": "abc123"
-    },
-}
-
-s3 = boto3.client("s3")
-s3_stub = Stubber(s3)
-expected_params = {
-    "Bucket": TEST_BUCKET,
-    "Key": TEST_KEY,
-}
-# s3_stub.add_response("put_object", put_response, expected_params)
-s3_stub.add_response("get_object", get_response)
-s3_stub.activate()
-
-
 class TestBlobLoader:
+    @pytest.fixture(autouse=True)
+    def setup_method(self):
+        self.s3_client = boto3.client("s3")
+        self.stubber = Stubber(self.s3_client)
+        self.stubber.activate()
 
-    # @pytest.mark.asyncio
-    # async def test_upload_blob(self):
-    #     blob_loader = AWSBlobLoader(s3)
-    #     upload_response = await blob_loader.upload_blob(TEST_BLOB_URL, TEST_BLOB)
-    #     assert upload_response == put_response
+        self.blob_loader = AWSBlobLoader(self.s3_client)
+
+    def teardown_method(self):
+        self.stubber.deactivate()
+
+    @pytest.mark.asyncio
+    async def test_aws_blob_loader_without_s3_client(self):
+        expired_time = datetime.now(timezone.utc) - timedelta(minutes=10)
+        CREDENTIALS_CACHE['expiration'] = expired_time
+        mock_s3_client = MagicMock()  
+        
+        with patch('app.providers.dependencies.aws.blob_loader.datetime') as mock_datetime, \
+            patch('app.providers.dependencies.aws.blob_loader.refresh_credentials') as mock_refresh_credentials:
+            mock_datetime.now.return_value = datetime.now(timezone.utc)
+            mock_refresh_credentials.return_value = None 
+            CREDENTIALS_CACHE["s3_client"] = mock_s3_client 
+            
+            loader = AWSBlobLoader()
+            
+            mock_refresh_credentials.assert_called_once()
+            
+            assert loader.s3_client == mock_s3_client
+
+    @pytest.mark.asyncio
+    async def test_upload_blob(self):
+        expected_params = {
+            "Bucket": TEST_BUCKET,
+            "Key": TEST_KEY,
+            "Body": TEST_BLOB
+        }
+        put_response = {
+            "ETag": "6805f2cfc46c0f04559748bb039d69ae",
+            "VersionId": "Bvq0EDKxOcXLJXNo_Lkz37eM3R4pfzyQ",
+            "ResponseMetadata": {
+                "RequestId": "abc123",
+                "HTTPStatusCode": 200,
+                "HostId": "abc123"
+            },
+        }
+        self.stubber.add_response("put_object", put_response, expected_params)
+        upload_response = await self.blob_loader.upload_blob(TEST_BLOB_URL, TEST_BLOB)
+
+        assert upload_response == put_response
 
     @pytest.mark.asyncio
     async def test_download_blob(self):
-        blob_loader = AWSBlobLoader(s3)
-        download_response = await blob_loader.download_blob(TEST_BLOB_URL)
-        print(download_response)
+        expected_message = {
+            "message": "blob"
+        }
+
+        encoded_message = json.dumps(expected_message).encode()
+        raw_stream = StreamingBody(
+            io.BytesIO(encoded_message),
+            len(encoded_message)
+        )
+        get_response = {
+            "Body": raw_stream
+        }
+        self.stubber.add_response("get_object", get_response)
+        download_response = await self.blob_loader.download_blob(TEST_BLOB_URL)
         assert download_response == encoded_message
