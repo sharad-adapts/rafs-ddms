80d4be4533e0ff5dc2aa44d6897eec93ba28cda0
Ernesto Gutierrez <ernesto_gutierrez@epam.com>
2024-12-03 14:43:45 +0000

    Issue-380: Add pydantic model gen scripts

---
+++ scripts/process_schemas.py
@@ -0,0 +1,85 @@
+#  Copyright 2024 ExxonMobil Technology and Engineering Company
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import argparse
+import json
+import os
+
+
+def update_additional_properties(properties):
+    """Set additionalProperties to False."""
+
+    for _, value in properties.items():
+        if isinstance(value, dict):
+            # Recursively process nested properties
+            if "properties" in value:
+                update_additional_properties(value["properties"])
+                if not "additionalProperties" in value:
+                    value["additionalProperties"] = False
+
+            # Handle arrays of objects
+            elif value.get("type") == "array" and "items" in value:
+                items = value["items"]
+                if isinstance(items, dict):
+                    if "properties" in items:
+                        update_additional_properties(items["properties"])
+                        if not "additionalProperties" in items:
+                            items["additionalProperties"] = False
+                elif isinstance(items, list):
+                    for elem in items:
+                        if "properties" in elem:
+                            update_additional_properties(elem["properties"])
+                        if not "additionalProperties" in elem:
+                            elem["additionalProperties"] = False
+
+                    # If there’s only one type of item, simplify the array
+                    if len(items) == 1:
+                        value["items"] = items[0]
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict) and "properties" in item:
+                    update_additional_properties(item["properties"])
+
+
+def process_schema_file(file_path):
+    with open(file_path, "r") as file:
+        schema = json.load(file)
+
+    # Update the properties in the schema
+    if "properties" in schema:
+        update_additional_properties(schema["properties"])
+        if not "additionalProperties" in schema:
+            schema["additionalProperties"] = False
+
+    # Write the updated schema back to the file
+    with open(file_path, "w") as file:
+        json.dump(schema, file, indent=2)
+
+
+def process_directory(directory_path: str, end_filter: str = "Schema.1.0.0.json"):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(end_filter):
+            file_path = os.path.join(directory_path, filename)
+            process_schema_file(file_path)
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Process JSON schema files in a specified directory.")
+    parser.add_argument("directory", type=str, help="Path to the directory containing JSON schema files.")
+    args = parser.parse_args()
+
+    # Call the function with the provided directory path
+    process_directory(args.directory)
